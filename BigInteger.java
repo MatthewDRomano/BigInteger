458acad923a38128b2fc436b2492f9b5241354e5
Matthew Romano <mromano592@gmail.com>
2023-12-03 00:48:00 -0500

    Multiplication v1 (errors present)

--- BigInteger.java
+++ BigInteger.java
@@ -26,7 +26,7 @@ public class BigInteger {
         for (int i = larger.digitList.size()-1; i >= 0; i--) {
             int dig1 = 0, dig2 = 0;
             if (iterSmall.hasNext())
-                dig1 = iterSmall.next() & 0xff; 
+                dig1 = iterSmall.next() & 0xff;
                 
             if (iterLarge.hasNext())
                 dig2 = iterLarge.next() & 0xff;
@@ -40,6 +40,40 @@ public class BigInteger {
             carry = (dig1 + dig2 + carry) / 10;
         }
         return new BigInteger(sum);
+    }
+
+    public BigInteger multiply(BigInteger mult) {
+        BigInteger smaller = (mult.digitList.size() <= this.digitList.size()) ? mult : this;
+        BigInteger larger = (mult.digitList.size() > this.digitList.size()) ? mult : this;
+
+        LinkedList<String> sumRows = new LinkedList<>();
+        int carry = 0;
+
+        Iterator<Byte> iterLarge;
+        Iterator<Byte> iterSmall = (smaller.digitList).descendingIterator();
+        
+        for (int i = 0; i < smaller.digitList.size(); i++) {
+            iterLarge = (larger.digitList).descendingIterator();
+            int dig1 = iterSmall.next() & 0xff;
+            String sum  = "";
+            for (int a = 0; a < i; a++) sum += "0";
+            for (int q = larger.digitList.size()-1; q >= 0; q--) {
+                int dig2 = iterLarge.next() & 0xff;
+                sum = ((dig1 * dig2 + carry) % 10) + sum;
+                carry = (dig1 * dig2 + carry) / 10;
+            }
+
+            System.out.println(Integer.parseInt(sum));
+            
+            sumRows.add(sum);
+        }
+
+        BigInteger ans = new BigInteger(sumRows.get(0));
+        for (int z = 1; z < sumRows.size(); z++) { //sumation doesnt work properly
+            ans = ans.add(new BigInteger(sumRows.get(z)));
+        }
+
+        return ans;
     }
 
     @Override
--- Client.java
+++ Client.java
@@ -1,11 +1,10 @@
 public class Client {
     public static void main(String[] args) {
         BigInteger one = new BigInteger("123");
-        BigInteger two = new BigInteger("345");
+        BigInteger two = new BigInteger("102");
 
         //BigInteger mistake = new BigInteger("bb3-j");
-
-        BigInteger sum = one.add(two);
+        BigInteger sum = one.multiply(two);
 
         System.out.println(sum);
     }
